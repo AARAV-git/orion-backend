4cc7ec9d4497b2d15eafbf30525f5a2acc7c3411
devs <saurav2282000@gmail.com>
2026-08-10 00:14:04 +0530

    ORION Updated

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,9 +1,10 @@
 # app/db/crud.py
+# pyrefly: ignore [missing-import]
 from sqlalchemy.orm import Session
 from app.db import models
 
 
-# ------------------ PATIENT ------------------
+
 
 def create_patient(db: Session, data: dict):
     patient = models.Patient(**data)
